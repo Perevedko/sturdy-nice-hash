7e7c34aedbbd2aa977d80c8d96f05779a357290f
Alexandr Rychagov <aarychagov@gmail.com>
2017-06-19 14:14:13 +0400

    upd

---
+++ coinwarz_proxy.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+import config
+import requests
+
+api_params = {
+    'apikey': config.coinwarz_api_key,
+    'algo': 'lyra2rev2',
+    # 'lyra2rev2HashRate': 1000,
+    # 'lyra2rev2Power': 0,
+    # 'lyra2rev2PowerCost': 0
+}
+COINWARZ_API_URL = 'http://www.coinwarz.com/v1/api/profitability/'
+
+def get_json():
+    return requests.get(COINWARZ_API_URL, params=api_params).json()
+
+def write_json():
+    with open('json.json', 'w') as f:
+        f.write(str(get_json()))
---
+++ mining_profitability.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+SECONDS_PER_DAY = 60 * 60 * 24
+SUPER_MAGIC_CONSTANT = 2 ** 32
+
+def profitability(
+    hashrate,
+    difficulty,
+    reward_per_block
+):
+
+    return (reward_per_block * hashrate * SECONDS_PER_DAY) / (difficulty * SUPER_MAGIC_CONSTANT)
